df785a18d9075077bc47a3ac962773d19093a283
Joel Papineau <118376438+j-papineau@users.noreply.github.com>
2023-07-23 16:53:50 -0400

    Working on Hero section and ani

--- portfolio/components/Hero.tsx
+++ portfolio/components/Hero.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Cursor, useTypewriter} from "react-simple-typewriter"
 import { Typewriter } from 'react-simple-typewriter'
+import BackgroundAnimation from './BackgroundAnimation'
+import Image from 'next/image'
+import Link from 'next/link'
 
 type Props = {}
 
@@ -13,22 +16,47 @@ function Hero({}: Props) {
     // });
 
   return (
-    <div>
+    <div className='h-screen flex flex-col space-y-4 items-center justify-center text-center overflow-hidden'>
+        <BackgroundAnimation/>
+        <Image className="relative rounded-full h-32 w-32 mx-auto object-cover" src="/images/logo.jpg" alt="logo"
+        height={400}
+        width={400}></Image>
 
+        <div className='z-20'>
+          <h2 className='text-xl uppercase font-extrabold text-slate-500 tracking-[10px]'>Joel Papineau | Dev</h2>
+          <h1 className='text-5xl lg:text-6xl'>
+              <p className='text-xl text-slate-400 font-serif font-semibold'>I Code For:</p>
+              <span className='text-slate-500 tracking-[4px]  mr-2'>
+                  <Typewriter
+                  loop
+                  cursor
+                  cursorStyle="_"
+                  words={["Myself", "The World", "Your Company"]}
+                  />
+              </span>
+          </h1>
+        </div>
+       
 
-        <h1>
-            <p className='text-2xl font-serif'>I Code For:</p>
-            <span style={{color: 'white', fontWeight: 'bold'}}>
-                <Typewriter
-                loop
-                cursor
-                cursorStyle="_"
-                words={["Myself", "The World", "Your Company"]}
-                />
-
-
-            </span>
-        </h1>
+        <div className="pt-5 z-20">
+          <Link href="#about">
+            <button className='heroButton'>About</button>
+          </Link>
+          <Link href="#skills">
+            <button className='heroButton'>Skills</button>
+          </Link>
+          <Link href="#experience">
+            <button className='heroButton'>Experience</button>
+          </Link>
+          
+          <Link href="#projects">
+            <button className='heroButton'>Projects</button>
+          </Link>
+          
+          
+        
+          
+        </div>
     
 
     </div>
--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -8,12 +8,12 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className='bg-[rgb(36,36,36)] text-white h-screen'>
+    <main className='bg-[rgb(20,0,20)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
       
       {/* Header */}
       <Header/>
       {/* Hero */}
-      <section id='hero'>
+      <section id='hero' className='snap-center'>
         <Hero/>
       </section>
       {/* About */}
